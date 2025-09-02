3d33b60a70fe03c5059d392ac3e75bfa4cddfc5b
Monica Dantas <mirella.a@escolar.ifrn.edu.br>
2025-09-02 18:23:42 -0300

    Conclusão do tópico Avatares do capítulo 6 do curso de Flask

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -10,12 +10,12 @@ from flask_login import logout_user
 from flask_login import login_required
 from flask import request
 from urllib.parse import urlsplit
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Acsa'}
     posts = [
     {
         'author': {'username': 'Theo'},
@@ -41,7 +41,7 @@ def login():
         user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('index'))
+            return redirect(url_for('login'))
         login_user(user, remember = form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or urlsplit(next_page).netloc != '':
@@ -53,3 +53,27 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username = form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title = 'Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
