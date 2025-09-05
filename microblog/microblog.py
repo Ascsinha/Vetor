4de335ad09fff609ac6d5cf62cb7932d61fbed87
Monica Dantas <mirella.a@escolar.ifrn.edu.br>
2025-09-05 16:35:15 -0300

    Conclusão dos capítulos 13 e 14 (as atualizações do capítulo 14 não funcionam) do curso de Flask

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,6 +1,6 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import app, db
+from app import app, db, cli
 from app.models import User, Post
 
 @app.shell_context_processor #Registra a função como uma função de comando do shell.
